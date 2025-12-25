5e4caaeb3d5c4dd8c2dcc1a779dc93f08427bbf2
EdithNancy <kouadioedith440@gmail.com>
2025-12-25 14:08:34 +0100

    First section ; Hero, implementation

---
+++ src/components/LinearBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LinearBar = ({ className = "" }) => {
+  return (
+    <div className={`w-full h-[0.1px] shadow-lg my-7 rounded-full overflow-hidden ${className}`}>
+      
+    </div>
+  )
+}
+
+export default LinearBar
---
+++ src/components/cards/TransparentCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const TransparentCard = ({ children, className = '' }) => {
+  return (
+    <div className={`
+      bg-white/5 backdrop-blur-lg border border-white/10 rounded-xl p-4 shadow-xl
+      ${className}
+    `}>
+      {children}
+    </div>
+  )
+}
+
+export default TransparentCard
