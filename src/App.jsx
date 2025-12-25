126f741f53bc48efa862146f74cede4ace8159e6
EdithNancy <kouadioedith440@gmail.com>
2025-12-25 13:57:04 +0100

    implementation of the introductory section of me : hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
-import { useState } from 'react'
 import './index.css'
-import Header from './components/Header'
 import Hero from './sections/Hero'
-import Button from './components/buttons/Button'
+import Header from './components/Header.jsx'
+
 
 function App() {
   return (
     <>
+    <div
+    className="flex min-h-screen bg-black text-white font-sans bg-fixed selection:bg-purple-500/30 relative bg-linear-to-b from-purple-950 via-black to-black">
+        <Header />
       <Hero/>
-      
-      <Header />
-      <Button
-            label="Télécharger CV"
-            onClick={() => {
-              // Logique de téléchargement du CV
-            }}
-          />   
+    </div>
+        
     </>
   )
 }
